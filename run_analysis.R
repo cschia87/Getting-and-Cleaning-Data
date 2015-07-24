b95cb3fa44b4edf3e107b80076e3032e3240d908
cschia87 <cschia87@gmail.com>
2015-07-24 17:08:34 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -3,17 +3,17 @@
 
 # 1. Merges the training and the test sets to create one data set.
 
-tmp1 <- read.table("train/X_train.txt")
-tmp2 <- read.table("test/X_test.txt")
-X <- rbind(tmp1, tmp2)
+t1 <- read.table("train/X_train.txt")
+t2 <- read.table("test/X_test.txt")
+X <- rbind(t1, t2)
 
-tmp1 <- read.table("train/subject_train.txt")
-tmp2 <- read.table("test/subject_test.txt")
-S <- rbind(tmp1, tmp2)
+t1 <- read.table("train/subject_train.txt")
+t2 <- read.table("test/subject_test.txt")
+S <- rbind(t1, t2)
 
-tmp1 <- read.table("train/y_train.txt")
-tmp2 <- read.table("test/y_test.txt")
-Y <- rbind(tmp1, tmp2)
+t1 <- read.table("train/y_train.txt")
+t2 <- read.table("test/y_test.txt")
+Y <- rbind(t1, t2)
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 
